df72b49531548d47e8e9f8cdb2d5c14f474d6300
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-07-15 12:05:43 -0300

    Criando um dockerfile para a aplicação e um docker-compose para a infra

--- codeblog/src/main/java/io/github/kalilventura/codeblog/repository/CodeBlogRepository.java
+++ codeblog/src/main/java/io/github/kalilventura/codeblog/repository/CodeBlogRepository.java
@@ -2,7 +2,9 @@ package io.github.kalilventura.codeblog.repository;
 
 import io.github.kalilventura.codeblog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CodeBlogRepository  extends JpaRepository<Post, Long> {
 
 }
